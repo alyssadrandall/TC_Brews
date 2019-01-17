868ab5592d3b1ad0328ca2400b5af1330599df23
Alyssa Kennedy <alyssadrandall@gmail.com>
2019-01-17 22:04:16 +0000

    updated bin file

--- lib/TC_Brews.rb
+++ lib/TC_Brews.rb
@@ -1,5 +1,8 @@
-require 'pry'
-require 'json'
-require 'rest-client'
+# require 'pry'
+# require 'json'
+# require 'rest-client'
 
-require "TC_Brews/version"
+require_relative "TC_Brews/version"
+require_relative "TC_Brews/CLI"
+require_relative "TC_Brews/Brewery"
+require_relative "TC_Brews/API"
